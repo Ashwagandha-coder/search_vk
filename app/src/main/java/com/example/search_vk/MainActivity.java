5d1c771b56bfe7f34e99e577b1925914cc8535a3
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-18 15:24:41 +0500

    base logic button

--- app/src/main/java/com/example/search_vk/MainActivity.java
+++ app/src/main/java/com/example/search_vk/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.search_vk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+    private EditText search_field;
+    private Button search_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // reference
+
+        result = findViewById(R.id.tv_result);
+        search_field = findViewById(R.id.et_search_field);
+        search_button = findViewById(R.id.btn_search);
+
+        // ovveride onclick
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText("Изменения");
+            }
+        };
+
+        search_button.setOnClickListener(onClickListener);
+
+
     }
 }
